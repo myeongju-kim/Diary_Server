115d77f77759f1bd4904afc4c8d6d702639d907d
myeongju-kim <mjoo1106@naver.com>
2022-12-27 15:36:54 +0900

    feat: add login function

--- src/main/java/com/kingmj/api/account/controller/AccountController.java
+++ src/main/java/com/kingmj/api/account/controller/AccountController.java
@@ -1,10 +1,26 @@
 package com.kingmj.api.account.controller;
 
+import com.kingmj.api.account.dto.LoginRequest;
+import com.kingmj.api.account.dto.LoginResponse;
+import com.kingmj.api.account.service.AccountService;
+import com.kingmj.api.common.dto.ServerResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/account")
 public class AccountController {
-
+  private final AccountService accountService;
+  public AccountController(AccountService accountService) {
+    this.accountService = accountService;
+  }
+  @Operation(summary = "로그인", description = "로그인을 진행합니다.")
+  @PostMapping("/login")
+  public ResponseEntity<ServerResponse<LoginResponse>> userLogin(@RequestBody LoginRequest loginRequest){
+    return ResponseEntity.status(200).body(accountService.login(loginRequest));
+  }
 }
--- src/main/java/com/kingmj/api/account/service/AccountService.java
+++ src/main/java/com/kingmj/api/account/service/AccountService.java
@@ -1,8 +1,37 @@
 package com.kingmj.api.account.service;
 
+import com.kingmj.api.account.dto.LoginRequest;
+import com.kingmj.api.account.dto.LoginResponse;
+import com.kingmj.api.common.code.ErrorCode;
+import com.kingmj.api.common.dto.ServerResponse;
+import com.kingmj.api.common.exception.UnauthorizedException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
-
+  @Value("${login.information.army}")
+  private String armyAnswer;
+  @Value("${login.information.gun}")
+  private String gunAnswer;
+  @Value("${login.information.door}")
+  private String doorAnswer;
+  private final JwtService jwtService;
+  private ServerResponse serverResponse=new ServerResponse();
+  public AccountService(JwtService jwtService) {
+    this.jwtService = jwtService;
+  }
+  public ServerResponse login(LoginRequest loginRequest){
+    String[] correct={armyAnswer,gunAnswer,doorAnswer};
+    Integer type=loginRequest.getType();
+    String answer=loginRequest.getAnswer();
+    if(type>2){
+      throw new UnauthorizedException(ErrorCode.INVALID_QUESTION);
+    }
+    if(!answer.equals(correct[type])){
+      throw new UnauthorizedException(ErrorCode.INVALID_ANSWER);
+    }
+    LoginResponse loginResponse=new LoginResponse(jwtService.createToken());
+    return serverResponse.success("로그인에 성공했습니다.",loginResponse);
+  }
 }
