41f82e06768fa042f3b49977b6508d5da28c17ba
myeongju-kim <mjoo1106@naver.com>
2022-12-27 12:32:16 +0900

    feat: create account controller

---
+++ src/main/java/com/kingmj/api/account/controller/AccountController.java
@@ -0,0 +1,10 @@
+package com.kingmj.api.account.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/account")
+public class AccountController {
+
+}
