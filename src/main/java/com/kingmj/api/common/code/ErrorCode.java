c5fbb115722099b4772cb0b1178da3872e8fe104
myeongju-kim <mjoo1106@naver.com>
2022-12-27 12:47:00 +0900

    feat: add unauthorized error codes

--- src/main/java/com/kingmj/api/common/code/ErrorCode.java
+++ src/main/java/com/kingmj/api/common/code/ErrorCode.java
@@ -1,5 +1,18 @@
 package com.kingmj.api.common.code;
 
-public enum ErrorCode {
+import lombok.Getter;
 
+@Getter
+public enum ErrorCode {
+  // HTTP Status : 401 (Unauthorized)
+  EXPIRE_TOKEN("U000", "토큰이 만료되었습니다."),
+  INVALID_TOKEN("U001", "토큰이 유효하지않습니다."),
+  INVALID_ACCESS("U002", "등록되지 않은 사용자입니다.");
+
+  private String code;
+  private String message;
+  ErrorCode(String code, String message) {
+    this.code=code;
+    this.message=message;
+  }
 }
