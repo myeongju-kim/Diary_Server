90b774d24ff82cb3b671241de77ca6c3cc2b1937
myeongju-kim <mjoo1106@naver.com>
2023-02-13 14:22:23 +0900

    fix: modify uri endpoint

--- src/main/java/com/kingmj/api/account/controller/AccountController.java
+++ src/main/java/com/kingmj/api/account/controller/AccountController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/account")
+@RequestMapping("/api/v1/accounts")
 public class AccountController {
   private final AccountService accountService;
   public AccountController(AccountService accountService) {
