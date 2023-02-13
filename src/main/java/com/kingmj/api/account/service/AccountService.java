ab88441823321311a1c95f233a53857d4e1a7c0f
myeongju-kim <mjoo1106@naver.com>
2023-02-13 13:17:13 +0900

    fix: delete login information (gun)

--- src/main/java/com/kingmj/api/account/service/AccountService.java
+++ src/main/java/com/kingmj/api/account/service/AccountService.java
@@ -3,7 +3,7 @@ package com.kingmj.api.account.service;
 import com.kingmj.api.account.dto.LoginRequest;
 import com.kingmj.api.account.dto.LoginResponse;
 import com.kingmj.api.common.code.ErrorCode;
-import com.kingmj.api.common.dto.ServerResponse;
+import com.kingmj.api.common.dto.ApiResponse;
 import com.kingmj.api.common.exception.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,17 +12,15 @@ import org.springframework.stereotype.Service;
 public class AccountService {
   @Value("${login.information.army}")
   private String armyAnswer;
-  @Value("${login.information.gun}")
-  private String gunAnswer;
   @Value("${login.information.door}")
   private String doorAnswer;
   private final JwtService jwtService;
-  private ServerResponse serverResponse=new ServerResponse();
+  private ApiResponse apiResponse =new ApiResponse();
   public AccountService(JwtService jwtService) {
     this.jwtService = jwtService;
   }
-  public ServerResponse login(LoginRequest loginRequest){
-    String[] correct={armyAnswer,gunAnswer,doorAnswer};
+  public ApiResponse login(LoginRequest loginRequest){
+    String[] correct={armyAnswer,doorAnswer};
     Integer type=loginRequest.getType();
     String answer=loginRequest.getAnswer();
     if(type>2){
@@ -32,6 +30,6 @@ public class AccountService {
       throw new UnauthorizedException(ErrorCode.INVALID_ANSWER);
     }
     LoginResponse loginResponse=new LoginResponse(jwtService.createToken());
-    return serverResponse.success("로그인에 성공했습니다.",loginResponse);
+    return apiResponse.success("로그인에 성공했습니다.",loginResponse);
   }
 }
