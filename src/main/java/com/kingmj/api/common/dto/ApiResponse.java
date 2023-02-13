0b79afe2508334cbcfc69778d64108a9c04c750c
myeongju-kim <mjoo1106@naver.com>
2023-02-13 14:21:38 +0900

    feat: modify api response

--- src/main/java/com/kingmj/api/common/dto/ApiResponse.java
+++ src/main/java/com/kingmj/api/common/dto/ApiResponse.java
@@ -1,35 +1,13 @@
 package com.kingmj.api.common.dto;
 
-import com.kingmj.api.common.code.ErrorCode;
+import com.kingmj.api.common.code.ServerCode;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ApiResponse<T> {
   private String code;
   private String message;
   private T result;
-  public ApiResponse() {
-    this.code = "S000";
-  }
-  // 요청에 성공 - 리턴 값이 존재함
-  public ApiResponse success(String message, T result){
-    this.message=message;
-    this.result=result;
-    return this;
-  }
-  // 요청에 성공 - 리턴 값이 존재하지 않음
-  public ApiResponse success(String message){
-    this.message=message;
-    this.result=null;
-    return this;
-  }
-  // 요청에 실패
-  public ApiResponse(ErrorCode code) {
-    this.message = code.getMessage();
-    this.code = code.getCode();
-    this.result = null;
-  }
-  public static ApiResponse of(ErrorCode code) {
-    return new ApiResponse(code);
-  }
 }
