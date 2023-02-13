45c8a9f3d1df6c9b96463dc0e357f945d8c7f2e0
myeongju-kim <mjoo1106@naver.com>
2023-02-13 14:43:43 +0900

    feat: create note table (jpa)

---
+++ src/main/java/com/kingmj/api/note/entity/Note.java
@@ -0,0 +1,25 @@
+package com.kingmj.api.note.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Note {
+    @Id @GeneratedValue
+    private Long id;
+    private String title;
+    @Column(length = 2000)
+    private String content;
+    private Integer weather;
+    private Integer mood;
+    private LocalDateTime date;
+}
