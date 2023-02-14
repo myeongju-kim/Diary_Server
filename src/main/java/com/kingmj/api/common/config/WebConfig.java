18a9bb2c6b727352908aeb556aef98c7f55af3b9
myeongju-kim <mjoo1106@naver.com>
2023-02-14 14:25:01 +0900

    feat: create web mvc configuration

---
+++ src/main/java/com/kingmj/api/common/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.kingmj.api.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,PATCH";
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(ALLOWED_METHOD_NAMES);
+    }
+}
