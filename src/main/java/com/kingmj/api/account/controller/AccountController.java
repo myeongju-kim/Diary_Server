92eb655cdd6adda5a8adf816e0973df67f5dda41
myeongju-kim <mjoo1106@naver.com>
2023-02-14 14:24:34 +0900

    fix: change from * to detail annotation

--- src/main/java/com/kingmj/api/account/controller/AccountController.java
+++ src/main/java/com/kingmj/api/account/controller/AccountController.java
@@ -6,10 +6,7 @@ import com.kingmj.api.account.service.AccountService;
 import com.kingmj.api.common.dto.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/accounts")
