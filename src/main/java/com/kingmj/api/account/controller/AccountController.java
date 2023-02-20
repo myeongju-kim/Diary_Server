21bdd79d8aeecb0feb05de1d9ccf58b1653b3b3d
myeongju-kim <mjoo1106@naver.com>
2023-02-20 17:40:45 +0900

    feat: create web token check function/api

--- src/main/java/com/kingmj/api/account/controller/AccountController.java
+++ src/main/java/com/kingmj/api/account/controller/AccountController.java
@@ -5,6 +5,8 @@ import com.kingmj.api.account.dto.LoginResponse;
 import com.kingmj.api.account.service.AccountService;
 import com.kingmj.api.common.dto.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import lombok.Getter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,5 +21,10 @@ public class AccountController {
   @PostMapping("/login")
   public ResponseEntity<ApiResponse<LoginResponse>> userLogin(@RequestBody LoginRequest loginRequest){
     return ResponseEntity.status(200).body(accountService.login(loginRequest));
+  }
+  @Operation(summary = "인증", description = "로그인이 되었는지 인증합니다.")
+  @GetMapping("/login")
+  public ResponseEntity<ApiResponse<Void>> userCheck(@RequestHeader(name="Authorization") String token){
+    return ResponseEntity.status(200).body(accountService.check(token));
   }
 }
