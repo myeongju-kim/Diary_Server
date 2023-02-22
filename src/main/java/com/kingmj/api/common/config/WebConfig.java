2ecea51b1c1859ffee3b07527c26c7cc155347d0
myeongju-kim <mjoo1106@naver.com>
2023-02-22 12:04:20 +0900

    feat: modify from '*' to environment

--- src/main/java/com/kingmj/api/common/config/WebConfig.java
+++ src/main/java/com/kingmj/api/common/config/WebConfig.java
@@ -1,16 +1,18 @@
 package com.kingmj.api.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,PATCH";
+    @Value("${cors.range}")
+    private String originRange;
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods(ALLOWED_METHOD_NAMES);
+                .allowedOrigins(originRange)
+                .allowedMethods("*");
     }
 }
