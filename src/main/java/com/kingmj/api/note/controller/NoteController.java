37021b2a6b83688db629901ccde2c2938ea1f2bb
myeongju-kim <mjoo1106@naver.com>
2023-02-22 12:45:12 +0900

    feat: create load notes function

--- src/main/java/com/kingmj/api/note/controller/NoteController.java
+++ src/main/java/com/kingmj/api/note/controller/NoteController.java
@@ -3,10 +3,14 @@ package com.kingmj.api.note.controller;
 import com.kingmj.api.account.service.JwtService;
 import com.kingmj.api.common.dto.ApiResponse;
 import com.kingmj.api.note.dto.NoteRequest;
+import com.kingmj.api.note.dto.NoteResponse;
 import com.kingmj.api.note.service.NoteService;
+import com.kingmj.api.util.Auth;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/note")
@@ -18,9 +22,14 @@ public class NoteController {
         this.jwtService = jwtService;
     }
     @PostMapping("")
-    public ResponseEntity<ApiResponse<Void>> postNote(@RequestHeader(name="Authorization") String token, @RequestBody NoteRequest.Create data){
-        jwtService.validateToken(token);
+    @Auth
+    public ResponseEntity<ApiResponse<Void>> postNote(@RequestBody NoteRequest.Create data){
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(noteService.postNote(data));
+    }
+    @GetMapping("")
+    public ResponseEntity<ApiResponse<List<NoteResponse.Load>>> getNote(@RequestParam(name="page") Integer page){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(noteService.getNote(page));
     }
 }
