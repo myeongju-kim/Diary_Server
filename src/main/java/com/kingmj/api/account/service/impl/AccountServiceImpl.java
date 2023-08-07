b734ad014075278500be8313afe44f6a934fc8f7
김명주 <kingmj@daou.co.kr>
2023-08-07 15:02:59 +0900

    fix: 인터페이스 관련 패키지로 이동

---
+++ src/main/java/com/kingmj/api/account/service/impl/AccountServiceImpl.java
@@ -0,0 +1,46 @@
+package com.kingmj.api.account.service.impl;
+
+import com.kingmj.api.account.dto.LoginRequest;
+import com.kingmj.api.account.dto.LoginResponse;
+import com.kingmj.api.account.service.AccountService;
+import com.kingmj.api.common.code.ServerCode;
+import com.kingmj.api.common.dto.ApiResponse;
+import com.kingmj.api.common.exception.UnauthorizedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountServiceImpl implements AccountService {
+
+    @Value("${login.information.army}")
+    private String armyAnswer;
+
+    @Value("${login.information.door}")
+    private String doorAnswer;
+
+    private final JwtServiceImpl jwtService;
+
+    @Override
+    public LoginResponse getTokenIfLoginSucceeds(LoginRequest loginRequest) {
+
+        String[] correct = {armyAnswer, doorAnswer};
+        loginRequest.getType();
+
+        Integer type = loginRequest.getType();
+        String answer = loginRequest.getAnswer();
+        // index error
+        if (type > 1) {
+            throw new UnauthorizedException(ServerCode.INVALID_QUESTION);
+        }
+        // invalid answer
+        if (!answer.equals(correct[type])) {
+            throw new UnauthorizedException(ServerCode.INVALID_ANSWER);
+        }
+
+        return LoginResponse.builder()
+                            .token(jwtService.createToken())
+                            .build();
+    }
+}
