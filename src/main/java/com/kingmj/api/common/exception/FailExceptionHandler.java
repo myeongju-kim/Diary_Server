c284f87982566c6e4e0fb724d0a303dc75f958b4
김명주 <kingmj@daou.co.kr>
2023-08-08 10:03:53 +0900

    feat: bad exception, server exception 추가

--- src/main/java/com/kingmj/api/common/exception/FailExceptionHandler.java
+++ src/main/java/com/kingmj/api/common/exception/FailExceptionHandler.java
@@ -1,10 +1,8 @@
 package com.kingmj.api.common.exception;
 
-import com.kingmj.api.common.dto.ApiResponse;
-import com.kingmj.api.common.dto.ApiResponseEntity;
+import com.kingmj.api.common.code.ServerCode;
+import com.kingmj.api.util.ApiResponseEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,5 +13,15 @@ public class FailExceptionHandler {
     @ExceptionHandler(UnauthorizedException.class)
     public ApiResponseEntity<Void> unauthorizedHandle(UnauthorizedException e) {
         return ApiResponseEntity.error(HttpStatus.UNAUTHORIZED, e.getServerCode());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ApiResponseEntity<Void> badRequestHandle(BadRequestException e){
+        return ApiResponseEntity.error(HttpStatus.BAD_REQUEST, e.getServerCode());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ApiResponseEntity<Void> exceptionHandle(Exception e){
+        return ApiResponseEntity.error(HttpStatus.INTERNAL_SERVER_ERROR, ServerCode.INTERNAL_SERVER_ERROR);
     }
 }
